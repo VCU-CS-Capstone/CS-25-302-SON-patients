3ffb27251623482bebb2f4a0302ceb4064243af9
IahhMoments <166156207+IahhMoments@users.noreply.github.com>
2025-02-27 17:00:37 -0500

    isaiah commit

--- src/patients-app/app/(tabs)/index.tsx
+++ src/patients-app/app/(tabs)/index.tsx
@@ -1,33 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { React } from "react";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Clinician Login</Text>
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Password"
+      />
+
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 40,
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  input: {
+    width: "67%",
+    height: "50px",
+    backgroundColor: "white",
+    marginTop: "30px",
+    padding: 10,
+    fontSize: 30,
+    color: "grey",
   },
 });
