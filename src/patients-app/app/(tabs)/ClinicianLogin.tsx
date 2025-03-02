8df234d1fa798442dc490926e358fd6a3bf0c715
IahhMoments <166156207+IahhMoments@users.noreply.github.com>
2025-03-01 19:19:56 -0500

    changed backgroundColor to hex color from figma

--- src/patients-app/app/(tabs)/ClinicianLogin.tsx
+++ src/patients-app/app/(tabs)/ClinicianLogin.tsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "green",
+    backgroundColor: "#B9CE88",
   },
   title: {
     fontSize: 40,
---
+++ src/patients-app/app/(tabs)/patientSearch.tsx
@@ -0,0 +1,9 @@
+import {View} from "react-native";
+
+export default function patientSearch() {
+    return (
+        <View style={StyleSheet.View}>
+            
+        </View>
+    );
+}
